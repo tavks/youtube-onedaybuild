08b4c8c164cb0b9ab7b983eb836d73f75ef65137
khramov <khramov.dev@gmail.com>
2020-10-11 12:26:30 +0300

    initial xcode project creation

---
+++ youtube-onedaybuild/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube-onedaybuild
+//
+//  Created by Владимир Храмов on 11.10.2020.
+//  Copyright © 2020 Владимир Храмов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
